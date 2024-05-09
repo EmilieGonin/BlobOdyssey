d900bfbb2aca2f7e260feff6538a2ce56cc9a35e
Emilie Gonin <emilie.yuu@gmail.com>
2024-05-09 15:53:44 +0200

    add asteroid random position

--- Assets/Scripts/Entities/Asteroid.cs
+++ Assets/Scripts/Entities/Asteroid.cs
@@ -1,4 +1,36 @@
-public class Asteroid : Entity
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
 {
-    //
+    private void Awake()
+    {
+        SetPositionOutsideViewport();
+    }
+
+    private void SetPositionOutsideViewport()
+    {
+        Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
+
+        int randomSide = Random.Range(0, 4);
+        Vector3 spawnPosition = Vector3.zero;
+
+        switch (randomSide)
+        {
+            case 0: // bottom
+                spawnPosition = new Vector3(Random.Range(bottomLeft.x, topRight.x), bottomLeft.y - 1, 0);
+                break;
+            case 1: // top
+                spawnPosition = new Vector3(Random.Range(bottomLeft.x, topRight.x), topRight.y + 1, 0);
+                break;
+            case 2: // left
+                spawnPosition = new Vector3(bottomLeft.x - 1, Random.Range(bottomLeft.y, topRight.y), 0);
+                break;
+            case 3: // right
+                spawnPosition = new Vector3(topRight.x + 1, Random.Range(bottomLeft.y, topRight.y), 0);
+                break;
+        }
+
+        transform.position = spawnPosition;
+    }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance => _instance;
+    public static GameManager Instance { get; private set; }
 
+    [Header("Modules")]
     [SerializeField] private List<Module> _modules = new();
 
     [Header("Prefabs")]
     [SerializeField] private GameObject _blobPrefab;
     [SerializeField] private GameObject _asteroidPrefab;
 
+    public Blob Blob { get; private set; }
+
     public GameObject BlobPrefab => _blobPrefab;
     public GameObject AsteroidPrefab => _asteroidPrefab;
 
-    private static GameManager _instance;
     private int _initializedModules = 0;
 
     public T Mod<T>() where T : Module => _modules.OfType<T>().First();
 
     private void Awake()
     {
-        if (_instance != null && _instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
             return;
         }
 
-        _instance = this;
+        Instance = this;
         DontDestroyOnLoad(gameObject);
 
         Module.OnModuleLoaded += Module_OnModuleLoaded;
@@ -47,12 +49,13 @@ public class GameManager : MonoBehaviour
 
     private void CompleteInit()
     {
+        SpawnBlob();
         Mod<WavesModule>().StartWave();
         Debug.Log("<color=yellow>Game Manager init completed.</color>");
     }
 
     private void SpawnBlob()
     {
-        //
+        Blob = Instantiate(BlobPrefab).GetComponent<Blob>();
     }
 }
