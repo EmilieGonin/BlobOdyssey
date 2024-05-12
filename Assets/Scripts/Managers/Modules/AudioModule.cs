d4fc48d682d7537fda401ff0831645788d073a6a
Emilie Gonin <emilie.yuu@gmail.com>
2024-05-12 14:00:48 +0200

    add audio module + destroy sound

--- Assets/Scripts/Managers/Modules/AudioModule.cs
+++ Assets/Scripts/Managers/Modules/AudioModule.cs
@@ -1,4 +1,24 @@
+using UnityEngine;
+
 public class AudioModule : Module
 {
-    //
+    [SerializeField] AudioSource _source;
+
+    [Header("Audio Clips")]
+    [SerializeField] AudioClip _destroy;
+
+    private void Awake()
+    {
+        DestroyAction.OnActivate += DestroyAction_OnActivate;
+    }
+
+    private void OnDestroy()
+    {
+        DestroyAction.OnActivate -= DestroyAction_OnActivate;
+    }
+
+    private void DestroyAction_OnActivate()
+    {
+        _source.PlayOneShot(_destroy);
+    }
 }
